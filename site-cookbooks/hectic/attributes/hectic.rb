ec6a7d824ae5f0f6b93cf2b84afa407c9d4abdf5
Matthew Todd <matthew.todd@gmail.com>
2009-09-05 13:30:45 +0300

    Revert "Don't generate passwords, as chef-solo won't remember them."
    This reverts commit a56ea2eb596abea5199b2fcc66905f6edf53a70c.
    
    It seems chef-solo scans all the available cookbooks, not just those specified
    in the json file. So we don't want to blow up.

--- site-cookbooks/hectic/attributes/hectic.rb
+++ site-cookbooks/hectic/attributes/hectic.rb
@@ -1,6 +1,3 @@
-raise('hectic["server_password"] is required!') unless hectic.server_password
-raise('hectic["db"]["password"] is required!')  unless hectic.db.password
-
 set.hectic.deploy_to            = '/var/www/apps/hectic'
 set.hectic.revision             = '99a4f76f791b3277c5c1ec1df2a54cb135803ddd'
 set.hectic.server_password_file = "#{hectic.deploy_to}/shared/passwd"
@@ -9,8 +6,10 @@ set_unless.hectic.repository      = 'git://github.com/matthewtodd/hectic.git'
 set_unless.hectic.server_name     = fqdn
 set_unless.hectic.server_aliases  = []
 set_unless.hectic.server_username = 'admin'
+set_unless.hectic.server_password = random_password(8)
 set_unless.hectic.environment     = 'production'
 
 set_unless.hectic.db.database     = 'hectic'
 set_unless.hectic.db.username     = 'hectic'
+set_unless.hectic.db.password     = random_password
 set_unless.hectic.db.environment  = hectic.environment
