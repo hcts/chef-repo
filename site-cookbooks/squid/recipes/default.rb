8105d996c11ca06a2739d06992b02066773e8644
Matthew Todd <matthew.todd@gmail.com>
2009-09-06 15:12:02 +0300

    Squid configuration.

--- site-cookbooks/squid/recipes/default.rb
+++ site-cookbooks/squid/recipes/default.rb
@@ -1,1 +1,11 @@
 package 'squid'
+
+service 'squid'
+
+template '/etc/squid/squid.conf' do
+  source 'squid.conf.erb'
+  owner 'root'
+  group 'root'
+  mode 0600
+  notifies :restart, resources(:service => 'squid')
+end
