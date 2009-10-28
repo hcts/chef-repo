0fe1477ea4eb72d7e3aab43fb881e86293ce7b03
Matthew Todd <matthew.todd@gmail.com>
2009-10-28 13:33:26 +0300

    First cut at bracken, for tailing logfiles.

---
+++ site-cookbooks/bracken/recipes/default.rb
@@ -0,0 +1,11 @@
+gem_package 'bracken' do
+  source 'http://gemcutter.org'
+  version '0.1.0'
+end
+
+template '/etc/bracken.rb' do
+  source 'bracken.rb.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+end
