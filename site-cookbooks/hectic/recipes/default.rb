f432fc4f1a81244f9edb7b6a475ebe84ed8335da
Matthew Todd <matthew.todd@gmail.com>
2010-04-30 14:15:20 +0300

    Backup and restore all MySQL databases.

--- site-cookbooks/hectic/recipes/default.rb
+++ site-cookbooks/hectic/recipes/default.rb
@@ -22,11 +22,6 @@ mysql_database node[:hectic][:db][:database] do
   password node[:hectic][:db][:password]
 end
 
-rsnapshot_backup_mysql_database node[:hectic][:db][:database] do
-  username node[:hectic][:db][:username]
-  password node[:hectic][:db][:password]
-end
-
 # FIXME Chef::Resource::Template will only accept a Hash, not a Chef::Node::Attribute
 database_configuration_hash = Hash.new
 node[:hectic][:db].each_attribute { |k,v| database_configuration_hash[k]=v }
