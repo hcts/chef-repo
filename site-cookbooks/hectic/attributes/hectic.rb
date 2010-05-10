b442acfe9ed01f0387900291f702375afa7708df
Matthew Todd <matthew.todd@gmail.com>
2010-05-10 09:26:05 +0300

    Moved hectic repo.

--- site-cookbooks/hectic/attributes/hectic.rb
+++ site-cookbooks/hectic/attributes/hectic.rb
@@ -1,7 +1,7 @@
 set.hectic.deploy_to = '/var/www/apps/hectic'
 set.hectic.revision  = '72a49460aed6a2cbfe964b9f8bee80c8ae2a2ca8'
 
-set_unless.hectic.repository      = 'git://github.com/matthewtodd/hectic.git'
+set_unless.hectic.repository      = 'git://github.com/hcts/hectic.git'
 set_unless.hectic.server_name     = fqdn
 set_unless.hectic.server_aliases  = []
 set_unless.hectic.environment     = 'production'
