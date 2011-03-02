8abc181b719398d2d39219f36466078f08b6429b
Matthew Todd <matthew@matthewtodd.org>
2011-03-02 10:31:08 +0300

    Bump smtp_data_xfer_timeout to 600s.

--- site-cookbooks/postfix/attributes/postfix.rb
+++ site-cookbooks/postfix/attributes/postfix.rb
@@ -3,8 +3,16 @@ set_unless.postfix.default_relayhost_username = ''
 set_unless.postfix.default_relayhost_password = ''
 set_unless.postfix.root_mail_to               = ''
 
+# In late February 2011, outgoing messages started piling up in the deferred
+# queue, with messages in the logs like "conversation with
+# smtp.strato.de[81.169.145.133] timed out while sending message body" and
+# "Timeout, closing transmission channel (in reply to end of DATA command)". By
+# tweaking these parameters, we're hoping to avoid those issues.
+#
+# From http://serverfault.com/questions/55949, I'm starting with bumping
+# smtp_data_xfer_timeout to 600s.
 set_unless.postfix.smtp_data_done_timeout = '600s' # postfix default: 600s
 set_unless.postfix.smtp_data_init_timeout = '120s' # postfix default: 120s
-set_unless.postfix.smtp_data_xfer_timeout = '180s' # postfix default: 180s
+set_unless.postfix.smtp_data_xfer_timeout = '600s' # postfix default: 180s
 
 set.postfix.virtual_mailbox_base = '/var/mail/virtual'
