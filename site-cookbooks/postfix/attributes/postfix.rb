f0c99466a65d1edfff6260a2fc6b9294d72da77f
Matthew Todd <matthew@matthewtodd.org>
2011-03-02 10:21:14 +0300

    Make SMTP sending timeouts configurable.

--- site-cookbooks/postfix/attributes/postfix.rb
+++ site-cookbooks/postfix/attributes/postfix.rb
@@ -3,4 +3,8 @@ set_unless.postfix.default_relayhost_username = ''
 set_unless.postfix.default_relayhost_password = ''
 set_unless.postfix.root_mail_to               = ''
 
+set_unless.postfix.smtp_data_done_timeout = '600s' # postfix default: 600s
+set_unless.postfix.smtp_data_init_timeout = '120s' # postfix default: 120s
+set_unless.postfix.smtp_data_xfer_timeout = '180s' # postfix default: 180s
+
 set.postfix.virtual_mailbox_base = '/var/mail/virtual'
